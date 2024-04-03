a62412294bc76dddb47d398ac407eadab9afdbc9
Tamar226 <tamarlicht3@gmail.com>
2024-04-03 14:39:53 +0300

    more...

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,5 +1,5 @@
 const dotenv = require('dotenv');
-const postsDataBase=require('../../database/postsHandlerDB')
+const postsDataBase = require('../../database/postsHandlerDB')
 dotenv.config();
 
 const express = require('express');
@@ -8,9 +8,12 @@ const router = express.Router();
 router.get('/', async (req, res) => {
     console.log('hiii');
     try {
-        const allPosts = await postsDataBase.getAllPosts();
-        if (result.hasError){
+        const result = await postsDataBase.getAllPosts();
+        if (result.hasError) {
             res.status(404).send('Error');
+        }
+        else {
+            res.status(200).send('success get all posts');
         }
         res.send(allPosts);
     } catch (error) {
@@ -22,51 +25,58 @@ router.get('/', async (req, res) => {
 router.get('/:postId', async (req, res) => {
     const postId = req.params.postId;
     try {
-        const post = await postsDataBase.getPostById(postId);
-        if (result.hasError){
+        const result = await postsDataBase.getPostById(postId);
+        if (result.hasError) {
             res.status(404).send('Error');
+        }
+        else {
+            res.status(200).send(`success get post by id: ${postId}`);
         }
         res.send(post);
     } catch (error) {
         console.error(`Error retrieving post with ID ${postId}:`, error);
         res.status(500).send('Internal Server Error');
     }
-    
+
 });
 
 router.post('/', async (req, res) => {
     const newPost = req.body;
     try {
-        const postId = await postsDataBase.addPost(newPost);
-        res.status(201).send(`post added with ID: ${postId}`);
+        const result = await postsDataBase.addPost(newPost);
+        if (result.insertId > 0) {
+            res.status(201).send(`Todo added with ID: ${todoId}`);
+        } else {
+            res.status(404).send('Error adding todo');
+        }
     } catch (error) {
         console.error('Error adding post:', error);
         res.status(500).send('Internal Server Error');
     }
 });
 
-  
-  router.put('/:postId', async (req, res) => {
+
+router.put('/:postId', async (req, res) => {
     const postId = req.params.postId;
     const updatedPostData = req.body;
     try {
-      const rowsAffected = await postsDataBase.updatePost(postId, updatedPostData);
-      if (rowsAffected > 0) {
-        res.status(200).send(`Post with ID ${postId} updated successfully`);
-      } else {
-        res.status(404).send(`Post with ID ${postId} not found`);
-      }
+        const result = await postsDataBase.updatePost(postId, updatedPostData);
+        if (result.affectedRows > 0) {
+            res.status(200).send(`Post with ID ${postId} updated successfully`);
+        } else {
+            res.status(404).send(`Post with ID ${postId} not found`);
+        }
     } catch (error) {
-      res.status(500).send('Internal Server Error');
+        res.status(500).send('Internal Server Error');
     }
-  });
-  
+});
+
 
 router.delete('/:postId', async (req, res) => {
     const postId = req.params.postId;
     try {
-        const rowsAffected = await postsDataBase.deletePost(postId);
-        if (rowsAffected > 0) {
+        const result = await postsDataBase.deletePost(postId);
+        if (result.affectedRows > 0) {
             res.status(200).send(`Post with ID ${postId} deleted successfully`);
         } else {
             res.status(404).send(`Post with ID ${postId} not found`);
